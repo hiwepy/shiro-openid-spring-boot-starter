d7cbe7fcc7843ac16092df4d22d6a8a5bfbb40f2
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:14:35 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/openid/realm/OpenidStatefulAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/openid/realm/OpenidStatefulAuthorizingRealm.java
@@ -5,7 +5,7 @@ import org.apache.shiro.spring.boot.openid.token.OpenidAccessToken;
 
 /**
  * Openid Stateful AuthorizingRealm
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class OpenidStatefulAuthorizingRealm extends AbstractAuthorizingRealm{
 
