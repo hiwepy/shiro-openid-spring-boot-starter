99e58b0140c2a17d1073689b33eb3e4511beaa88
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:14:14 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/openid/token/OpenidAccessToken.java
+++ src/main/java/org/apache/shiro/spring/boot/openid/token/OpenidAccessToken.java
@@ -19,7 +19,7 @@ import org.apache.shiro.authc.HostAuthenticationToken;
 
 /**
  * KISSO Access Token
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @SuppressWarnings("serial")
 public class OpenidAccessToken implements HostAuthenticationToken {
